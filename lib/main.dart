c8c734b2ece9b17f2a76574d5a2b214f1f87c571
timilehinjegede <timilehintimothy.jegede@gmail.com>
2020-05-24 01:17:53 +0100

    Set the device orientation to portrait up only

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
 import 'package:notesapp/database/database.dart';
 import 'package:notesapp/models/note.dart';
@@ -7,40 +8,44 @@ import 'package:notesapp/screens/home.dart';
 import 'package:notesapp/screens/new_note.dart';
 import 'package:path_provider/path_provider.dart';
 
+
+Future openBox() async {
+  Box _noteBox;
+  Hive.registerAdapter(
+    NoteAdapter(),
+  );
+  var dir = await getApplicationDocumentsDirectory();
+  Hive.init(dir.path);
+  _noteBox = await Hive.openBox('noteBox');
+}
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   void initState() {
     // TODO: implement initState
+    openBox();
     super.initState();
-    _openBox();
-  }
-
-  Future _openBox() async{
-    Box _noteBox;
-    Hive.registerAdapter(
-      NoteAdapter(),
-    );
-    var dir = await getApplicationDocumentsDirectory();
-    Hive.init(dir.path);
-    _noteBox = await Hive.openBox('noteBox');
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        NewNoteScreen.routeName: (context) => NewNoteScreen(note: Note(),action: 'Add Note',),
+        NewNoteScreen.routeName: (context) => NewNoteScreen(
+              note: Note(),
+              action: 'Add Note',
+            ),
       },
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
